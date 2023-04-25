c7839cec823dfcdee0fb5c1fc03311cfe58a4bf7
Sebastian Kärner <sebastiankarner@live.se>
2023-04-25 12:01:48 +0200

    Using different types of implementations for loading lists etc.

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,23 +1,53 @@
-
-import React from 'react';
+import React, {useState} from 'react';
 import usePost from "./hooks/usePost";
+import 'bootstrap';
 
 const PostList = () => {
 
-  const {data: posts, error, isLoading} = usePost()
+    //const [userId, setUserId] = useState<number>() kommentera ut för fitrering på user
+    const pageSize = 10;
+    //const [page, setPage] = useState<number>(1) För prev,next knappar!
+    const {data, error, isLoading, fetchNextPage, isFetchingNextPage} = usePost(/*page,*/ {pageSize}/*userId*/);
 
-  if(isLoading) return <p>Loading...</p>
-  if (error) return <p>{error.message}</p>;
+    if (isLoading) return <p>Loading...</p>
+    if (error) return <p>{error.message}</p>;
 
-  return (
-    <ul className="list-group">
-      {posts?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <>
+            {/* <select onChange={(event) => setUserId(parseInt(event.target.value))} FILTRERA PÅ USER
+                    value={userId}
+                    className="form-select mb-3">
+                <option value=''></option>
+                <option value='1'>User 1</option>
+                <option value='2'>User 2</option>
+                <option value='3'>User 3</option>
+            </select> */}
+            <ul className="list-group">
+                {data.pages.map((page, index) =>
+                    <React.Fragment key={index}>
+                        {page.map(post =>
+                            <li key={post.id} className="list-group-item">
+                                {post.title}
+                            </li>)}
+                    </React.Fragment>)}
+            </ul>
+            {/* <button
+                disabled={page === 1}
+                className="btn btn-primary my-3"
+                onClick={() => setPage(page -1)}>Previous
+            </button>
+            <button
+                disabled={page === 10}
+                className="btn btn-primary my-3 ms-2"
+                onClick={() => setPage(page +1)}>Next
+            </button>*/}
+            <button
+                disabled={isFetchingNextPage}
+                className="btn btn-primary my-3"
+                onClick={() => fetchNextPage()}>{isFetchingNextPage ? 'Loading...' : 'Load More'}
+            </button>
+        </>
+    );
 };
 
 export default PostList;
--- src/react-query/hooks/usePost.ts
+++ src/react-query/hooks/usePost.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {useQuery} from "@tanstack/react-query";
+import {useInfiniteQuery, useQuery} from "@tanstack/react-query";
 
 interface Post {
     id: number;
@@ -8,17 +8,30 @@ interface Post {
     userId: number;
 }
 
-const UsePost = () => {
+interface PostQuery {
+    // page: number; för prev,next knappar
+    pageSize: number;
+}
 
-    const fetchPosts = () =>
-    axios
-        .get('https://jsonplaceholder.typicode.com/posts')
-        .then((res) => res.data);
+const UsePost = (query: PostQuery/*userId: number | undefined*/) =>  // här skickar vi med userid för att filtrera på users
 
-    return useQuery<Post[], Error>({
-        queryKey: ['posts'],
-        queryFn: fetchPosts
+    useInfiniteQuery<Post[], Error>({
+        queryKey: ['posts', query] /*userId ? ['users', userId, 'posts'] : ['posts']*/, // turnery ifall vi väljer ingen user, så visas bara posts i query devtools
+        queryFn: ({pageParam = 1}) => axios // ifall vi kör infinite, pageparam skickas med för att hålla reda på vilken sida vi är på.
+            .get('https://jsonplaceholder.typicode.com/posts', {
+                params: {
+                    //userId userfiltrering
+                    _start: (/*query.page*/pageParam - 1) * query.pageSize,
+                    _limit: query.pageSize
+                }
+            })
+            .then((res) => res.data),
+        staleTime: 1 * 60 * 1000, // 1min
+        keepPreviousData: true, // detta gör så att inte sidan hoppar upp till start varje gång vi fetchar ny data.
+        getNextPageParam: (lastPage, allPages) => { // här skriver vi syntax för att kunna ladda nya sidor.
+            return lastPage.length > 0 ? allPages.length + 1 : undefined; // ifall listan är slut =0 så retuyrneras undefined.
+        }
     })
-};
+
 
 export default UsePost;
