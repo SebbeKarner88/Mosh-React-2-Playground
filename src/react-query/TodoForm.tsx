27fc9aa2b763f9a31b20a807089a652f7172e0ad
Sebastian Kärner <sebastiankarner@live.se>
2023-04-25 12:23:53 +0200

    implemented a Mutation hook for posting data to a API via Axios

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,18 +1,60 @@
-import { useRef } from 'react';
+import {useRef} from 'react';
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import {Todo} from "./hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
-  const ref = useRef<HTMLInputElement>(null);
 
-  return (
-    <form className="row mb-3">
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
-  );
+    const queryClient = useQueryClient();
+
+    const addTodo = useMutation({
+        mutationFn: (todo: Todo) =>
+            axios
+                .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+                .then(res => res.data),
+        onSuccess: (savedTodo, newTodo) => {
+            // 1 invalidating the cache, funkar dock inte med JSON placeholder.
+            //queryClient.invalidateQueries({
+              //  queryKey: ['todos']
+            //})
+
+            // 2 Updating the data in cache directly.
+            queryClient.setQueryData<Todo[]>(['todos'], todos => [savedTodo, ...(todos || [])])
+
+        }
+    });
+
+    const ref = useRef<HTMLInputElement>(null);
+
+    return (
+        <form className="row mb-3"
+              onSubmit={event => {
+                  event.preventDefault();
+
+                  if (ref.current && ref.current.value)
+                  addTodo.mutate({
+                      id: 0,
+                      title: ref.current.value,
+                      completed: false,
+                      userId: 1
+                  })
+              }}>
+            <div
+                className="col">
+                <input
+                    ref={ref}
+                    type="text"
+                    className="form-control"/>
+            </div>
+            <div
+                className="col">
+                <button
+                    className="btn btn-primary">
+                    Add
+                </button>
+            </div>
+        </form>
+    );
 };
 
 export default TodoForm;
