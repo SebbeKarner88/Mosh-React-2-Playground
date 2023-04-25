a6189b237787027bc52068188d42f8f04125b5f4
Sebastian Kärner <sebastiankarner@live.se>
2023-04-25 14:21:44 +0200

    implementing a optimistic update. with onsucess, onerror and onmutate

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,33 +3,54 @@ import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import {Todo} from "./hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContext {
+    previousTodos: Todo[]
+}
+
 const TodoForm = () => {
 
     const queryClient = useQueryClient();
 
-    const addTodo = useMutation<Todo, Error, Todo>({
+    const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
+
         mutationFn: (todo: Todo) =>
             axios
                 .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
                 .then(res => res.data),
-        onSuccess: (savedTodo, newTodo) => {
-            // 1 invalidating the cache, funkar dock inte med JSON placeholder.
-            //queryClient.invalidateQueries({
-            //  queryKey: ['todos']
-            //})
 
-            // 2 Updating the data in cache directly.
-            queryClient.setQueryData<Todo[]>(['todos'], todos => [savedTodo, ...(todos || [])]);
+        onMutate: (newTodo: Todo) => { // Uppdaterar så posten syns i listan direkt, sen kollar vi om det fungerade. OPTIMISTIC
 
-            if (ref.current) ref.current.value = ''; // tar bort värde ur input efter success post.
+            const previousTodos = queryClient.getQueryData<Todo[]>(['todos']) || []; // context objekt till onerror nedan
+
+            queryClient.setQueryData<Todo[]>(['todos'],
+                todos => [newTodo, ...(todos || [])]);
+
+            if (ref.current) ref.current.value = '';
+
+            return {previousTodos} // här returnerar vi context.
         },
+
+        onSuccess: (savedTodo, newTodo) => {
+
+            queryClient.setQueryData<Todo[]>(['todos'], // on success, så lägger posten till i listan i backend.
+                todos => todos?.map(todo =>
+                    todo === newTodo ? savedTodo : todo)) // här uppdaterar vi listan. om inte så oförändrad.
+        },
+
+        onError: (error, newTodo, context) => {
+
+            if(!context) return; // här kollar vi om context är truthy,
+
+            queryClient.setQueryData<Todo[]>(['todos'], context.previousTodos) // om det gick fel så tar vi bort posten och återgår till previous som vi har som context.
+        }
     });
 
     const ref = useRef<HTMLInputElement>(null);
 
     return (
         <>
-            {addTodo.error && <div className='alert alert-danger'>{addTodo.error.message}</div>} {/*mutate kan också hantera errors*/}
+            {addTodo.error &&
+                <div className='alert alert-danger'>{addTodo.error.message}</div>} {/*mutate kan också hantera errors*/}
             <form className="row mb-3"
                   onSubmit={event => {
                       event.preventDefault();
@@ -63,3 +84,16 @@ const TodoForm = () => {
 };
 
 export default TodoForm;
+
+
+// // ### OM VI BARA ANVÄNDER ONSUCCESS #####
+//             // 1 invalidating the cache, funkar dock inte med JSON placeholder.
+//             //queryClient.invalidateQueries({
+//             //  queryKey: ['todos']
+//             //})
+//
+//             // 2 Updating the data in cache directly.
+//             // queryClient.setQueryData<Todo[]>(['todos'], todos => [savedTodo, ...(todos || [])]);
+//
+//             //if (ref.current) ref.current.value = ''; // tar bort värde ur input efter success post.
+//             // ### OM VI BARA ANVÄNDER ONSUCCESS #####
