aef69acf3580302fa604fcb5e883680193c45adb
Sebastian Kärner <sebastiankarner@live.se>
2023-04-27 12:46:31 +0200

    broke out a todoservice

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,21 +1,12 @@
 import {useQuery} from "@tanstack/react-query";
-import APIClient from "../services/apiClient";
 import {CACHE_KEY_TODOS} from "../constants";
-
-const apiClient = new APIClient<Todo>('/todos')
-
-export interface Todo {
-    id: number;
-    title: string;
-    userId: number;
-    completed: boolean;
-}
+import todoService, {Todo} from "../services/todoService";
 
 const UseTodos = () => {
 
     return useQuery<Todo[], Error>({
         queryKey: CACHE_KEY_TODOS,
-        queryFn: apiClient.getAll,
+        queryFn: todoService.getAll,
         staleTime: 10*1000 // här kan vi sätta specifika configs som vi vill att just denna query ska ha.
     })
 };
